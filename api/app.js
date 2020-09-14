2518e1fa79ab34d34ab6ab37a0851f1f57ed2861
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-09-14 15:40:57 -0600

    got pretty far with okta

--- api/app.js
+++ api/app.js
@@ -4,6 +4,9 @@ import cors from "cors";
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import helmet from 'helmet';
+//i messed w/ this
+const { ExpressOIDC } = require('@okta/oidc-middleware');
+const session = require('express-session');
 
 import indexRouter from './routes/index';
 import profileRouter from './routes/profile';
@@ -21,6 +24,27 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//okta stuff??
+
+const oidc = new ExpressOIDC({
+  issuer: process.env.OKTA_URL_ISSUER,
+  client_id: process.env.OKTA_CLIENT_ID,
+  client_secret: process.env.OKTA_CLIENT_SECRET,
+  appBaseUrl: process.env.BASE_URL,
+  loginRedirectUri: `${process.env.BASE_URL}/callback`,
+  scope: 'openid profile',
+  routes: {
+    loginCallback: {
+      path: '/callback'
+    },
+  }
+});
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: false
+}));
+app.use(oidc.router);
 
 // application routes
 app.use('/', indexRouter);
