3d717350c3becdf5e770f8d92d96bee56a7421f7
Harrison <hmseaborn@gmail.com>
2020-11-04 08:47:29 -0800

    backend up

--- api/models/accountModel.js
+++ api/models/accountModel.js
@@ -1,49 +1,48 @@
-const db = require("../../data/db-config.js");
+const db = require('../../data/db-config.js');
 
 module.exports = {
-    findById,
-    findByEmail,
-    findByUsername,
-    add,
-    update,
-    updateById
-}
+  findById,
+  findByEmail,
+  findByUsername,
+  add,
+  update,
+  updateById,
+};
 
 function findById(id) {
-    // account_id is a UUID string
-    return db("account").where({ account_id: id }).first();
+  // account_id is a UUID string
+  return db('account').where({ account_id: id }).first();
 }
 
 function findByEmail(email) {
-    return db("account").where({ email: email }).first();
+  //   console.log(email);
+  return db('account').where({ email: email }).first();
 }
 
 function findByUsername(username) {
-    return db("account").where({ username: username }).first();
+  return db('account').where({ username: username }).first();
 }
 
 function add(new_account) {
-    // Use router to check that required info is included.
-    return db("account")
-        .insert(new_account, "account_id")
-        .then(([account_id]) => {
-            return findById(account_id)
-        })
+  // Use router to check that required info is included.
+  return db('account')
+    .insert(new_account, 'account_id')
+    .then(([account_id]) => {
+      return findById(account_id);
+    });
 }
 
 function update(new_data, email) {
-    // Before calling this function,
-    // verify in router that only appropriate data is included.
-    return db("account")
-        .where({ email: email })
-        .update(new_data)
-        .returning([account_id]);
+  // Before calling this function,
+  // verify in router that only appropriate data is included.
+  return db('account')
+    .where({ email: email })
+    .update(new_data)
+    .returning([account_id]);
 }
 
 function updateById(new_data, id) {
-    // Before calling this function,
-    // verify in router that only appropriate data is included.
-    return db("account")
-        .where({ account_id: id })
-        .update(new_data)
+  // Before calling this function,
+  // verify in router that only appropriate data is included.
+  return db('account').where({ account_id: id }).update(new_data);
 }
--- api/tools/hash_tools.js
+++ api/tools/hash_tools.js
@@ -1,11 +1,24 @@
 const bcrypt = require('bcryptjs');
 
-module.exports = {
-    hasher
-}
+const hasher = async (text, size) => {
+  try {
+    const salt = await bcrypt.genSalt(12);
 
-function hasher(pin) {
-    const rounds = process.env.HASH_ROUNDS || 12;
-    const hash = bcrypt.hashSync(pin, rounds);
+    const hash = await bcrypt.hash(text, salt);
+
     return hash;
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  hasher,
+};
+// const bcrypt = require('bcryptjs');
+
+// function hasher(pin, rounds) {
+//   const rounds = process.env.HASH_ROUNDS || 12;
+//   const hash = bcrypt.hashSync(pin, rounds);
+//   return hash;
+// }
