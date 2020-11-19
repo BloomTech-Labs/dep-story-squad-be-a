be2ae1fd3e9f2fce59d4cae8fdb248e824147ce9
Harrison <hmseaborn@gmail.com>
2020-11-19 09:51:42 -0800

    cleaning up commented out code

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,11 @@
-var dotenv = require('dotenv');
+var dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
   development: {
-    client: 'pg',
-    migrations: { directory: './data/migrations' },
-    seeds: { directory: './data/seeds' },
+    client: "pg",
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
     pool: {
       min: 2,
       max: 10,
@@ -14,64 +14,16 @@ module.exports = {
   },
 
   test: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: "../data/migrations" },
+    seeds: { directory: "../data/seeds" },
   },
 
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: "../data/migrations" },
+    seeds: { directory: "../data/seeds" },
   },
 };
-
-// // // Update with your config settings.
-
-// module.exports = {
-
-//   development: {
-//     client: 'pg',
-//     connection: {
-//       filename: './dev.sqlite3'
-//     },
-//     migrations: { directory: './data/migrations' }
-//   },
-
-//   staging: {
-//     client: 'pg',
-//     connection: {
-//       database: 'my_db',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       directory: './data/migrations',
-//       tableName: 'knex_migrations'
-//     }
-//   },
-
-//   production: {
-//     client: 'pg',
-//     connection: {
-//       database: 'my_db',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       directory: './data/migrations',
-//       tableName: 'knex_migrations'
-//     }
-//   }
-
-// };
